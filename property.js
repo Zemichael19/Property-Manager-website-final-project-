bd98ed8319f2bce1080a1906b8a9d98f31572acd
Arman Tavana <armantavana@Armans-MacBook-Pro.local>
2020-04-03 15:27:32 -0400

    property

--- property.js
+++ property.js
@@ -1,1 +1,14 @@
-hello
+
+// Model for the course collection.
+const mongoose = require('mongoose');
+
+// Define the schema
+const Course = new mongoose.Schema({
+  _id: String,
+  title: String,
+  description: String,
+  prereqs: [String]
+});
+
+// Export the model
+module.exports = mongoose.model('Course', Course);
