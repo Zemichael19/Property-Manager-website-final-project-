be8972e8fea9cfb1e73af19ab4312d9e52981bc5
Arman Tavana <armantavana@Armans-MBP.home>
2020-04-04 14:07:33 -0400

    csa

---
+++ apartment.js
@@ -0,0 +1,22 @@
+// Model for the apartment collection.
+const mongoose = require('mongoose');
+
+// Define the schema
+const Apartment = new mongoose.Schema({
+    n_rooms: Number,
+    n_bathrooms: Number,
+    listed: Boolean,
+    furnished:Boolean,
+    floor:Number,
+    sqFoot:Number,
+    pets: Boolean,
+    laundry: Boolean,
+    parking: Boolean,
+    s_date: Date,
+    e_date: Date,
+    ac: Boolean,
+    u_num: Number
+});
+
+// Export the model
+module.exports = mongoose.model('Apartment', Apartment);
