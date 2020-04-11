c139b9b95668a80b1d4dc936f136dc37b41e6008
Arman Tavana <armantavana@Armans-MacBook-Pro.local>
2020-04-11 15:39:37 -0400

    dedcw

--- controllers/properties.js
+++ controllers/properties.js
@@ -23,14 +23,3 @@ module.exports.retrieve = function(request, response, next) {
     }
   }).catch(error => next(error));
 };
-// Controller for the section collection.
-const Apartment = require('../models/apartment');
-
-// GET /sections?sort=
-module.exports.index = function(request, response, next) {
-  const order = request.query.sort || 'u_num'; // Default to sort by unit number
-
-  Apartment.find().sort(order)
-    .then(apartments => response.render('properties/index', {apartments: apartments, order: order}))
-    .catch(error => next(error));
-};
