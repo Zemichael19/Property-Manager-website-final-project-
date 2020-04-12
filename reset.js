bcd3b60aff9213d0a5019905daeadf33b22f1b68
Arman Tavana <armantavana@Armans-MacBook-Pro.local>
2020-04-12 18:29:35 -0400

    changing input

--- reset.js
+++ reset.js
@@ -18,7 +18,7 @@ const properties = [
 
 const apartments = [
   new Apartment({property: properties[0].id, n_rooms: 3, n_bathrooms: 2,listed: false, furnished:true, floor: 4,sqFoot:1400,pets: true,laundry: false,parking: true,s_date: 'June 2018' ,e_date: 'June 2021',ac: false,u_num: 410}),
-  new Apartment({property: properties[0].id, n_rooms: 2, n_bathrooms: 8,listed: true, furnished:true, floor: 2,sqFoot:2300,pets: false,laundry: true,parking: false,s_date: 'January 2017' ,e_date: 'January 2020',ac: true,u_num: 209}),
+  new Apartment({property: properties[0].id, n_rooms: 2, n_bathrooms: 1,listed: true, furnished:true, floor: 2,sqFoot:2300,pets: false,laundry: true,parking: false,s_date: 'January 2017' ,e_date: 'January 2020',ac: true,u_num: 209}),
   new Apartment({property: properties[1].id, n_rooms: 1, n_bathrooms: 1,listed: false, furnished:false, floor: 3,sqFoot:800,pets: true,laundry: true,parking: true,s_date: 'October 2019' ,e_date: 'April 2020',ac: true,u_num: 303}),
   new Apartment({property: properties[1].id, n_rooms: 2, n_bathrooms: 2,listed: true, furnished:true, floor: 1,sqFoot:1200,pets: false,laundry: true,parking: false,s_date: 'April 2019' ,e_date: 'April 2020',ac: false,u_num: 101}),
   new Apartment({property: properties[1].id, n_rooms: 1, n_bathrooms: 1,listed: false, furnished:false, floor: 1,sqFoot:900,pets: true,laundry: false,parking: true,s_date: 'May 2019' ,e_date: 'May 2020',ac: true,u_num: 110}),
