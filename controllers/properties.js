943a20c44f8f74d8739ac9b70cbe5b032a6a7338
Zemichael Habtegiorgis <zemichaelhabtegiorgis@Zemichaels-MacBook-Pro.local>
2020-04-11 18:21:21 -0600

    combining apartment and properties page

--- controllers/properties.js
+++ controllers/properties.js
@@ -1,6 +1,9 @@
 // Controller for the properties collection.
 const Property = require('../models/property');
 
+// Controller for the section collection.
+const Apartment = require('../models/apartment');
+
 // GET /properties
 module.exports.index = function(request, response, next) {
   Property.distinct('_id')
@@ -8,16 +11,18 @@ module.exports.index = function(request, response, next) {
     .catch(error => next(error));
 };
 
-// GET /properties/:id
+// GET /properties/:id?sort
 module.exports.retrieve = function(request, response, next) {
+  const order = request.query.sort || 'u_num';
   const queries = [
     Property.findById(request.params.id),
-    Property.find()
+    Property.find(),
+    Apartment.find().where("property").equals(request.params.id).sort(order)
   ];
 
-  Promise.all(queries).then(function([property, properties]) {
+  Promise.all(queries).then(function([property, properties, apartments]) {
     if (property) {
-      response.render('properties/index', {property: property, properties: properties});
+      response.render('properties/index', {property: property, properties: properties, apartments:apartments, order:order});
     } else {
       next(); // No such property
     }
