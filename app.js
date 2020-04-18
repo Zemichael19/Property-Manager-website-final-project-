e8ef12f250e19d9046ae753745180479b3e7f4d4
Zemichael Habtegiorgis <zemichaelhabtegiorgis@Zemichaels-MacBook-Pro.local>
2020-04-18 14:17:51 -0600

    phase 2 test

--- app.js
+++ app.js
@@ -14,8 +14,6 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-// Parse request bodies like query strings
-app.use(express.urlencoded({extended: false}));
 
 
 // Ignore icon requests
@@ -36,23 +34,7 @@ app.get('/', function(request, response) {
   response.redirect('/properties');
 });
 
-// Enter admin mode and return to the previous page
-app.get('/login', function(request, response) {
-  request.session.admin = true;
-  response.redirect('back');
-});
 
-// Exit admin mode and return to the previous page
-app.get('/logout', function(request, response) {
-  request.session.admin = false;
-  response.redirect('back');
-});
-
-// Make the mode available in all views
-app.use(function(request, response, next) {
-  response.locals.admin = request.session.admin;
-  next();
-});
 
 // Route content requests
 app.use('/', router);
