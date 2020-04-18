53f08a35a9e23b820544cc7209274eae4f8a44bf
Zemichael Habtegiorgis <zemichaelhabtegiorgis@Zemichaels-MacBook-Pro.local>
2020-04-18 14:20:53 -0600

    phase 2 test

--- controllers/apartments.js
+++ controllers/apartments.js
@@ -9,24 +9,3 @@ module.exports.index = function(request, response, next) {
     .then(apartments => response.render('apartments/index', {apartments: apartments, order: order}))
     .catch(error => next(error));
 };
-
-// POST /sections (with the new section in the request body)
-module.exports.create = function(request, response, next) {
-  Apartment.create(request.body)
-    .then(apartment => response.status(201).send(apartment.id))
-    .catch(error => next(error));
-};
-
-// DELETE /sections/:id
-module.exports.delete = function(request, response, next) {
-  Apartment.findByIdAndDelete(request.params.id)
-    .then(apartment => apartment ? response.status(200).end() : next())
-    .catch(error => next(error));
-};
-
-// PUT /sections/:id (with the changes in the request body)
-module.exports.update = function(request, response, next) {
-  Section.findByIdAndUpdate(request.params.id, request.body)
-    .then(apartment => apartment ? response.status(200).end() : next())
-    .catch(error => next(error));
-};
