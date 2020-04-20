0ba6dbe0dcd57bf68d7930163303f56191ca2b15
Arman Tavana <armantavana@Armans-MacBook-Pro.local>
2020-04-20 00:36:07 -0400

    router?(need to ask question)

--- router.js
+++ router.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const properties = require('./controllers/properties');
 const apartments = require('./controllers/apartments');
-
+const users = require('./controllers/users');
 // Create the router
 const router = express.Router();
 
-// Check for admin status
-const authorize = function(request, response, next) {
-  if (request.session.admin) {
-    next(); // Fulfill the request
-  } else {
-    response.status(401).end();
-  }
-};
+// Handle home-page requests
+router.get('/', function(request, response) {
+  response.render('index');
+});
+
+// Handle login requests
+router.post('/login', users.login);
+
+// Handle logout requests
+router.get('/logout', function(request, response) {
+  request.session.user = undefined;
+  response.redirect('/');
+});
 
 // Handle property requests
 router.get('/properties', properties.index);
