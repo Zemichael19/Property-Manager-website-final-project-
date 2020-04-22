dffbe0b7a73152fc3be40511340df38a501c7142
Zemichael Habtegiorgis <zemichaelhabtegiorgis@Zemichaels-MacBook-Pro.local>
2020-04-22 11:33:46 -0600

    create update and delete

--- controllers/properties.js
+++ controllers/properties.js
@@ -30,23 +30,23 @@ module.exports.retrieve = function(request, response, next) {
   }).catch(error => next(error));
 };
 
-// POST /courses (with the new course in the request body)
+// POST /properties (with the new course in the request body)
 module.exports.create = function(request, response, next) {
   Property.create(request.body)
     .then(property => response.status(201).send(property.id))
     .catch(error => next(error));
 };
 
-// DELETE /courses/:id
+// DELETE /properties/:id
 module.exports.delete = function(request, response, next) {
   Property.findByIdAndDelete(request.params.id)
     .then(property => property ? response.status(200).end() : next())
     .catch(error => next(error));
 };
 
-// PUT /courses/:id (with the changes in the request body)
+// PUT /properties/:id (with the changes in the request body)
 module.exports.update = function(request, response, next) {
-  Course.findByIdAndUpdate(request.params.id, request.body)
+  Property.findByIdAndUpdate(request.params.id, request.body)
     .then(property => property ? response.status(200).end() : next())
     .catch(error => next(error));
 };
