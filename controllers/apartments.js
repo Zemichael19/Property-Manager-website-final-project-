f9bde953acbf9ae4e0537cf2220ba6871ee977b2
Rajat <rshar17@stlawu.edu>
2020-04-28 12:55:24 -0400

    apartments new

--- controllers/apartments.js
+++ controllers/apartments.js
@@ -1,11 +1,8 @@
 // Controller for the section collection.
 const Apartment = require('../models/apartment');
 
-// GET /apartments?sort=
-module.exports.index = function(request, response, next) {
-  const order = request.query.sort || 'u_num'; // Default to sort by unit number
 
-  Apartment.find().sort(order)
-    .then(apartments => response.render('apartments/index', {apartments: apartments, order: order}))
-    .catch(error => next(error));
-};
+module.exports.new = function(request, response, next)
+{
+  response.render('apartments/new');
+}
