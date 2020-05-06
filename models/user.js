e6f67f14dbca84575565871a85f25a3904d50076
Arman Tavana <armantavana@Armans-MacBook-Pro.local>
2020-05-06 17:15:14 -0400

    error fix

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 // Define the schema
 const User = new mongoose.Schema({
   _id: {type: String, required: true,minlength:5, maxlength: 100},
-  name:{type: String, required: true,minlength:3, maxlength: 100}
+  name:{type: String, required: true, maxlength: 100}
 });
 
 // Export the model
